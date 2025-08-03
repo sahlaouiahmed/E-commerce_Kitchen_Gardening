2ae70b348640839a9a92c775fb042d6362c86d13
Ahmed <sahlaouiahmad@outlook.com>
2025-08-03 14:29:05 +0200

    Updated product detail template with spacing and layout tweaks

--- kitchen_garden/settings.py
+++ kitchen_garden/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DEBUG', False)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
